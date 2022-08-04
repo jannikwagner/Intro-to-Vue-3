00cc530c44d3e82f7af3d539cfc42800ded778d8
Jannik Wagner <jannik.wagner@ambient.digital>
2022-08-04 12:39:47 +0200

    coding task

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data: function() {
+        return {
+            product: 'Socks',
+            description: 'You wear those on your feet.'
+        }
+    }
+})
